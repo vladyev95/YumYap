470d8593b423b2100ddcaf820f4d5fe6c1c40c17
vladyev95 <yevseenko.vladimir@gmail.com>
2017-09-19 08:53:26 -0400

    Created base html/js for login/register/app

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+(function() {
+    let app = angular.module('app', []);
+        
+    
+    /*
+     * controller for the overall application
+     * holds whether we are loggedIn or not
+     */
+    app.controller('AppController', function() {
+        this.loggedIn = false;
+
+        
+    });
+    
+    
+    /*
+     * controller for the Login/Register part of the app
+     * holds whether we are on the login page or the register page
+     */
+    app.controller('LoginController', function() {
+        this.onLogin = true;
+    });
+    
+    /*
+     * controller for the stuff once we've logged in
+     */
+    app.controller('ContentController', function() {
+        
+    });
+
+})();
