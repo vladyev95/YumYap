4ba341e65a67f4cab0c2639072abb619cdefa4c0
YC1984 <yangc12345@gmail.com>
2017-09-23 20:10:27 -0400

    ImageTest

--- testImg/src/main/webapp/js/testImg.js
+++ testImg/src/main/webapp/js/testImg.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	loadImg();
 };
-
+/*
 function loadImg(){
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function(){
@@ -17,4 +17,32 @@ function loadImg(){
 	}
 	xhr.open("GET", "load", true);
 	xhr.send();
-};
+};*/
+function loadImg(){
+	
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4 && xhr.status == 200){
+			var dto = JSON.parse(xhr.responseText);
+			var dto = JSON.parse(xhr.responseText);
+			var imgList = dto.imgs;
+			for(var i=imgList.length-1; i>=0; i--){
+				
+				var table = document.getElementById("imgList");
+				var row = table.insertRow();
+				var cake = row.insertCell(0);
+				var word = row.insertCell(1);
+				
+				var img = document.createElement("img");
+				img.src = imgList[i].link;
+				var src = document.getElementById("here");
+				
+				cake.appendChild(img);
+				word.innerHTML=imgList[i].description;
+			}
+		}
+	}
+	
+	xhr.open("GET", "load", true);
+	xhr.send();
+}
