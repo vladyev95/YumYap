ee8ea33bcdbecd9032ca04403a95de9b6e4309fe
koszuta <koszutan94@gmail.com>
2017-09-27 16:53:08 -0400

    Added basic DTOs

---
+++ YumYapProj/src/main/java/com/yumyap/dto/RecipeDto.java
@@ -0,0 +1,14 @@
+package com.yumyap.dto;
+
+public class RecipeDto {
+
+    private int id;
+    
+    private int creatorId;
+
+    private String name;
+
+    private String description;
+
+    private String imageUrl;
+}
---
+++ YumYapProj/src/main/java/com/yumyap/dto/SimpleUserDto.java
@@ -0,0 +1,10 @@
+package com.yumyap.dto;
+
+public class SimpleUserDto {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+}
