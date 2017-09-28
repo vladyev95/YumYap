165064204c3de4a9d0485ab78f7e5e37800b9323
YC1984 <yangc12345@gmail.com>
2017-09-28 14:08:51 -0400

    viewAuthor

---
+++ viewAuthor/YumYapProj/src/main/java/com/yumyap/beans/FoodItem.java
@@ -0,0 +1,105 @@
+package com.yumyap.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * An object representing the FoodItems that Recipes consist of
+ * @author vlad
+ */
+@Component
+@Entity
+@Table(name = "food_items")
+public class FoodItem {
+
+	@Id
+	@Column(name = "food_item_id")
+	@SequenceGenerator(name = "food_item_id_seq", sequenceName = "food_item_id_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "food_item_id_seq")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "measure")
+	private String measure;
+	
+	@Column(name = "amount")
+	private double amount;
+
+
+	public FoodItem() {
+	}
+
+	public FoodItem(String name, String measure, double amount) {
+		this.name = name;
+		this.measure = measure;
+		this.amount = amount;
+	}
+	
+	/**
+	 * Returns the id of this FoodItem
+	 * @return The id of this FoodItem
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id of this FoodItem
+	 * @param id The new id of this FoodItem
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Returns the name of the measure that this FoodItem uses
+	 * @return The name of the measure that this FoodItem uses
+	 */
+	public String getMeasure() {
+		return measure;
+	}
+
+	/**
+	 * Sets the name of the measure that this FoodItem uses
+	 * @param measure The new measure that this FoodItem uses
+	 */
+	public void setMeasure(String measure) {
+		this.measure = measure;
+	}
+
+	/**
+	 * Returns the amount of the measure for this FoodItem
+	 * @return The amount of the measure for this FoodItem
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * Sets the amount of the measure for this FoodItem
+	 * @param amount The new amount of measure for this FoodItem
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	/**
+	 * Returns a nice String representation of a FoodItem
+	 */
+	@Override
+	public String toString() {
+		return "FoodItem { id: " + id + 
+				", name: " + name +
+				", measure: " + measure + 
+				", amount: " + amount + " }";
+	}
+}
---
+++ viewAuthor/YumYapProj/src/main/java/com/yumyap/dao/Dao.java
@@ -0,0 +1,78 @@
+package com.yumyap.dao;
+
+import java.util.List;
+import java.util.Set;
+
+import com.yumyap.beans.Comment;
+import com.yumyap.beans.FoodItem;
+import com.yumyap.beans.Recipe;
+import com.yumyap.beans.RecipeDirection;
+import com.yumyap.beans.User;
+
+/**
+ * Interface representing a DAO (Data Access Object)
+ * Always use this interface as the variable type
+ * @author vlad
+ */
+public interface Dao {
+	
+	/**
+	 * Attempts to add a User to the persistent store
+	 * May fail if an User with the email already exists
+	 * @param user The user to attempt to add
+	 * @return Whether the User was successfully added
+	 */
+	boolean addUser(User user);
+	
+	FoodItem addFoodItem(FoodItem foodItem);
+	Comment addComment(Comment comment);
+	Recipe addRecipe(Recipe recipe);
+	RecipeDirection addRecipeDirection(RecipeDirection recipeDirection);
+	
+	/**
+	 * Attempts to retrieve a User with the given email and password
+	 * @param email The email of the User
+	 * @param password The password of the User
+	 * @return User upon success, null upon no such User existing
+	 */
+	User getUserByEmailAndPassword(String email, String password);
+	
+	/**
+	 * Returns The recipes by people the user with the given id follows
+	 * @param id The if of the User to display recipes to
+	 * @return All the recipes of people that the user with the id follows
+	 */
+	Set<Recipe> getFollowingRecipes(User user);
+
+	/**
+	 * Returns the Recipes that the user with the given id has created
+	 * @param id The id of the User whose recipes we are retrieving
+	 * @return The Set of Recipes created by this User
+	 */
+	Set<Recipe> getUsersRecipes(User user);
+	
+	/**
+	 * Returns the User with the corresponding id
+	 * @param id The id of the user to get
+	 * @return The User with the id or null if no such user
+	 */
+	User getUserById(int id);
+	
+	/**
+	 * Returns the Recipe with the specified id
+	 * @param id The id of the Recipe to get
+	 * @return The Recipe with the id, null if no such recipe
+	 */
+	Recipe getRecipeById(int id);
+
+	List<Recipe> getRecipesByUser(User user);
+
+	void updateUser(User user);
+	
+	/**
+	 * Adds the Comment to the Recipe with the corresponding id
+	 * @param id The id of the Recipe to add the comment to
+	 * @param comment The Comment to add to the Recipe with the corresponding id
+	 */ 
+	void addCommentForRecipeById(int id, Comment comment);
+}
